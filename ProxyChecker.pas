78fc719648bd456ee1cbf832b964676284257709
Jurij <yurij-bukatkin@yandex.ru>
2014-03-03 22:41:08 +0300

    Update ProxyChecker.pas

--- ProxyChecker.pas
+++ ProxyChecker.pas
@@ -1,51 +1,41 @@
-program Project1;
+program project1;
 
 {$mode objfpc}{$H+}
 
-uses
-HttpSend,regexpr,SysUtils,Classes;
-
-type
-TProxy = class
-private
-Http:THttpSend;
-reg:TRegExpr;
-public
-constructor Create;
-function Test(ip:String;port:String):boolean;
-end;
+uses laz_synapse,HttpSend,Sysutils,Classes,RegExpr;
 
 
-constructor TProxy.Create;
+
+var
+  reg:TRegExpr;
+  http:THttpSend;
+  FS:TStringList;
+  InputFile:TStringList;
+  i:integer;
+  Addr:String;
+  InText:integer;
 begin
-
-  http:=THttpSend.Create;
+  FS:=TStringList.Create;
   reg:=TregExpr.Create;
   reg.Expression:='d_clip_button">(.*?)\<';
+  InputFile:=TStringList.Create;
+  http:=Thttpsend.Create;
   http.UserAgent :='Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727)';
+  InputFile.LoadFromFile('/Users/mas/Documents/ProxyChecker/dHidder/proxy.txt');
+  for i:=0 to inputfile.Count-1 do begin
+      Addr:=InputFile.Strings[i];
+      InText:= Pos(':', Addr);
+      http.ProxyHost:= Copy(Addr, 0, InText - 1);
+      http.ProxyPort:= Copy(Addr, InText + 1, Length(Addr));
+      if http.HTTPMethod('get','2ip.ru') then  begin
+        FS.LoadFromStream(http.document);
+        if reg.Exec(FS.Text) then
+           if(Reg.Match[1])<>'' then Writeln(Reg.Match[1])
+                                else Writeln('NO');
+      end;
+
   end;
 
-function TProxy.Test(ip:string;port:string):boolean;
-var
-SL:TStringList;
-begin
-  SL:=TStringList.Create;
-  http.ProxyHost:=ip;
-  http.ProxyPort:=port;
-  http.HTTPMethod('get','2ip.ru');
-  Sl.LoadFromStream(http.document);
-  if reg.Exec(sl.Text) then
-    if(Reg.Match[1])<>'' then Result:=true
-                         else Result:=false;
-  sl.Free;
 
-end;
-
-
-var
-P:TProxy;
-begin
-p:=TProxy.Create;
-writeln(p.Test('202.116.1.149','8128'));
-p.Free;
+  readln;
 end.
